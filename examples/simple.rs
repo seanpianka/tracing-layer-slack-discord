071ad545b0624e709893bdf4fb9ee9670dca62c5
Sean Pianka <pianka@eml.cc>
2021-08-01 14:15:23 -0400

    add target filter + work queue
    Signed-off-by: Sean Pianka <pianka@eml.cc>

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,11 +1,10 @@
-use tokio::time::{Duration, sleep};
 use tracing::info;
 use tracing::instrument;
 use tracing_bunyan_formatter::JsonStorageLayer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::Registry;
 
-use tracing_layer_slack::SlackForwardingLayer;
+use tracing_layer_slack::{SlackForwardingLayer, SlackConfig};
 
 #[instrument]
 pub async fn a_unit_of_work(_first_parameter: u64) {
@@ -20,13 +19,17 @@ pub fn a_sub_unit_of_work(_sub_parameter: u64) {
     info!("Events have the full context of their parent span!");
 }
 
-#[tokio::main]
-async fn main() {
-    let slack_layer = SlackForwardingLayer::new_from_env();
-    let subscriber = Registry::default().with(JsonStorageLayer).with(slack_layer);
-    tracing::subscriber::set_global_default(subscriber).unwrap();
-
+pub async fn handler() {
     info!("Orphan event without a parent span");
     a_unit_of_work(2).await;
-    sleep(Duration::from_secs(5)).await;
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+    let worker_handle = tokio::spawn(tracing_layer_slack::worker(rx));
+    let slack_layer = SlackForwardingLayer::new("simple".into(), SlackConfig::default(), tx);
+    let subscriber = Registry::default().with(JsonStorageLayer).with(slack_layer);
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+    tokio::join!(worker_handle, handler());
 }
